0a8f96711eed253aded40df75d79aa806a3d4f02
msmith2919 <59847525+msmith2919@users.noreply.github.com>
2020-10-22 13:34:35 -0400

    commit 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,66 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
+export default class SimpleFormExample extends React.Component {
+    state = {
+        formData: {
+            name: '',
+            age: '',
         },
-    },
-}));
+        submitted: false,
+    }
 
-export default function BasicTextFields() {
-    const classes = useStyles();
+    handleChange = (event) => {
+        const { formData } = this.state;
+        formData[event.target.name] = event.target.value;
+        this.setState({ formData });
+    }
 
-    return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField
-                id="outlined-basic"
-                label="Name"
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-basic"
-                label="Age"
-                variant="outlined"
-            />
-        </form>
-    );
+    handleSubmit = () => {
+        this.setState({ submitted: true }, () => {
+            setTimeout(() => this.setState({ submitted: false }), 5000);
+        });
+    }
+
+    render() {
+        const { formData, submitted } = this.state;
+        return (
+            <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+            >
+                <h2>User Information Form</h2>
+                <TextValidator
+                    label="Name"
+                    onChange={this.handleChange}
+                    name="name"
+                    value={formData.name}
+                    validators={['matchRegexp:^[A-Za-z]+$']}
+                    errorMessages={['Enter more than one character, no numbers']}
+                />
+                <br />
+                <TextValidator
+                    label="Age"
+                    onChange={this.handleChange}
+                    name="age"
+                    value={formData.age}
+                    validators={['minNumber:10']}
+                    errorMessages={['Enter a number greater than 10']}
+                />
+                <br />
+                <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    disabled={submitted}
+                >
+                    {
+                        (submitted && 'Your form is submitted!')
+                        || (!submitted && 'Submit')
+                    }
+                </Button>
+            </ValidatorForm>
+        );
+    }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Box from "./Box";
-import style from "../style/style";
 
 function Home(){
-    const [toggle, setToggle] = React.useState("Hiya");
-    const [color, setColor]= React.useState("blue");
+    //const [toggle, setToggle] = React.useState("Hiya");
+    //const [color, setColor]= React.useState("blue");
 
-    const [boxInfo]=React.useState([{name:"Fred", age:69},{name:"Freddy", age:40},{name:"Frooty", age:10}]);
+    //const [boxInfo]=React.useState([{name:"Fred", age:69},{name:"Freddy", age:40},{name:"Frooty", age:10}]);
     //const [displayInfo, setInfo]=React.useState(boxInfo);
     //const [currentSort, setSort]=React.useState("all")
 
@@ -20,20 +18,9 @@ function Home(){
         }
 
     }, [currentSort]);*/
-
-    const boxEles = boxInfo.map((bx, idx)=>
-        <Box name={bx.name} age={bx.age} key={idx}/>
-    );
-
-    return (
-        <div className="homePage" style={{backgroundColor:color, color: "white"}}>
-            <div className="cool">
-                {toggle}
-            </div>
-            <div style={style.exampleBox}></div>
-            <button onClick={()=>setToggle("Hello there")}>Click Me</button>
-            <button onClick={()=>setColor("red")}>Change Color</button>
-            {boxEles}
+    return(
+        <div>
+            {"Welcome to Mason's Material UI web app"}
         </div>
     );
 }
